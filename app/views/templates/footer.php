3fb22d59f47121cbbfb8eddd94c097c85fbf7ddd
Md Mahbub  E Waduzzaman <45024227+waduzzaman@users.noreply.github.com>
2025-07-16 10:55:42 +0000

    update footer with new toast message

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -22,22 +22,46 @@
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 
-<?php if (isset($_SESSION['flash_message'])): ?>
 <script>
-  Toastify({
-    text: <?= json_encode($_SESSION['flash_message']) ?>,
-    duration: 3000,
-    gravity: "top",
-    position: "right",
-    backgroundColor: <?= json_encode($_SESSION['flash_color'] ?? "linear-gradient(to right, #4ade80, #22c55e)") ?>,
-    stopOnFocus: true
-  }).showToast();
-</script>
-<?php 
-unset($_SESSION['flash_message']); 
-unset($_SESSION['flash_color']);
-endif; ?>
+  <?php if (isset($_SESSION['toast_success'])): ?>
+    Toastify({
+      text: <?= json_encode($_SESSION['toast_success']) ?>,
+      duration: 4000,
+      gravity: "top",
+      position: "right",
+      backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+      stopOnFocus: true
+    }).showToast();
+    <?php unset($_SESSION['toast_success']); ?>
+  <?php endif; ?>
 
+  <?php if (isset($_SESSION['toast_error'])): ?>
+    Toastify({
+      text: <?= json_encode($_SESSION['toast_error']) ?>,
+      duration: 4000,
+      gravity: "top",
+      position: "right",
+      backgroundColor: "linear-gradient(to right, #ef4444, #f87171)",
+      stopOnFocus: true
+    }).showToast();
+    <?php unset($_SESSION['toast_error']); ?>
+  <?php endif; ?>
+
+  <?php if (isset($_SESSION['flash_message'])): ?>
+    Toastify({
+      text: <?= json_encode($_SESSION['flash_message']) ?>,
+      duration: 3000,
+      gravity: "top",
+      position: "right",
+      backgroundColor: <?= json_encode($_SESSION['flash_color'] ?? "linear-gradient(to right, #4ade80, #22c55e)") ?>,
+      stopOnFocus: true
+    }).showToast();
+    <?php 
+      unset($_SESSION['flash_message']);
+      unset($_SESSION['flash_color']);
+    ?>
+  <?php endif; ?>
+</script>
 
 </body>
 </html>
