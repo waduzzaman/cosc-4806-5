04de98239e085447dc7a28817f91498e8ab36cb9
Md Mahbub  E Waduzzaman <45024227+waduzzaman@users.noreply.github.com>
2025-07-16 10:27:56 +0000

    update login controller

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -6,12 +6,28 @@ class Login extends Controller {
 	    $this->view('login/index');
     }
     
-    public function verify(){
+	public function verify()
+	{
 			$username = $_REQUEST['username'];
 			$password = $_REQUEST['password'];
-		
+
 			$user = $this->model('User');
-			$user->authenticate($username, $password); 
-    }
+			$auth = $user->authenticate($username, $password); 
+
+			if ($auth) {
+					
+					$_SESSION['auth'] = true;
+					$_SESSION['username'] = $username;
+					$_SESSION['toast_success'] = "Login successful!";
+					header('Location: /home');
+					exit;
+			} else {
+					//  Track failed attempts here if needed
+					$_SESSION['toast_error'] = "Invalid username or password.";
+					header('Location: /login');
+					exit;
+			}
+	}
+
 
 }
